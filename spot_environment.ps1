baba3baa4caea2641e68281d8c59d9c2e8b2b9a8
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-09-12 18:14:28 -0400

    changes

--- spot_environment.ps1
+++ spot_environment.ps1
@@ -64,6 +64,16 @@ function Update-DNS($clientIP, $dnsIP) {
 # first computer is DNS
 $lab | select -skip 1 | % { Update-DNS $_.instance.PrivateIpAddress $lab[0].instance.PrivateIpAddress | Out-Null }
 
+"disable IEESC..." | Out-Default
+Invoke-Command -computer $lab.instance.PrivateIpAddress -script {
+  '{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}','{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' | % { Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\$_" -Name 'IsInstalled' -Value 0 }
+}
+
+"disable firewall..." | Out-Default
+Invoke-Command -computer $lab.instance.PrivateIpAddress -script {
+  netsh advfirewall set allprofiles state off
+}
+
 function Test-TcpPort($target, $port)
 {
     $ErrorActionPreference = 'SilentlyContinue'
