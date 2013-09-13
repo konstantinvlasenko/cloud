2463ef118afa486a2f3224b26fd0ae3373124755
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-09-13 18:29:50 -0400

    configure SecurityGroup

--- spot_environment.ps1
+++ spot_environment.ps1
@@ -17,7 +17,7 @@ $images = Get-EC2Image | ? OwnerId -eq 637921189560
 $role = Get-IAMInstanceProfileForRole S3Reader
 
 # create spot requests
-$lab | % { $_.request = Request-EC2SpotInstance -SpotPrice $_.maxbid -LaunchSpecification_InstanceType $_.type -LaunchSpecification_ImageId ($images | ? Name -eq $_.amiName).ImageId -LaunchSpecification_SecurityGroupId 'Fitnesse' -LaunchSpecification_InstanceProfile_Arn $role.Arn -LaunchSpecification_InstanceProfile_Id $role.InstanceProfileId }
+$lab | % { $_.request = Request-EC2SpotInstance -SpotPrice $_.maxbid -LaunchSpecification_InstanceType $_.type -LaunchSpecification_ImageId ($images | ? Name -eq $_.amiName).ImageId -LaunchSpecification_SecurityGroupId $config.SecurityGroup -LaunchSpecification_InstanceProfile_Arn $role.Arn -LaunchSpecification_InstanceProfile_Id $role.InstanceProfileId }
 
 "waiting for spot requests fulfilment..." | Out-Default
 do {
