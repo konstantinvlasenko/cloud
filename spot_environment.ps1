7388d0967812b8eda1f9d7fa5bb9f1679f944254
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-09-13 20:06:45 -0400

    Test-WSMan

--- spot_environment.ps1
+++ spot_environment.ps1
@@ -71,6 +71,14 @@ function Update-DNS($clientIP, $dnsIP) {
 # first computer is DNS
 $lab | select -skip 1 | % { Update-DNS $_.instance.PrivateIpAddress $lab[0].instance.PrivateIpAddress | Out-Null }
 
+"Tests whether the WinRM service is running on the clients..." | Out-Default
+do {
+  Sleep 60
+  $lab | ? { $_.WSMan -ne $true } | % { $_.WSMan = (Test-WSMan $_.name) -ne $null }
+  $lab | % { "[$($_.name)]`WSMan = $($_.WSMan)" | Out-Default }
+  
+} while( ($lab | ? { $_.WSMan -ne $true }) -ne $null )
+
 "disable IEESC..." | Out-Default
 Invoke-Command -computer $lab.instance.PrivateIpAddress -script {
   '{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}','{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}' | % { Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\$_" -Name 'IsInstalled' -Value 0 }
