593cb2d06459d8efdb7399d0f25cec225bc195d6
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2013-10-27 01:09:25 -0400

    change

--- fitnesse.ps1
+++ fitnesse.ps1
@@ -8,26 +8,29 @@ if($config.ssh -ne $null) {
 $cname = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/public-hostname -UseBasicParsing).Content
 $instanceId = (Invoke-WebRequest -Uri http://169.254.169.254/latest/meta-data/instance-id -UseBasicParsing).Content
 
+# set instance name
+New-EC2Tag -ResourceId $instanceId -Tag (new-object Amazon.EC2.Model.Tag).WithKey('Name').WithValue("ci.$($config.DomainName)")
+.\Register-CNAME.ps1 $config "ci.$($config.DomainName)" $cname
+.\Register-CNAME.ps1 $config "fitnesse.$($config.DomainName)" $cname
+
 # attach EBS volume with TeamCity data
 $filter = new-object Amazon.EC2.Model.Filter  
 $filter.Name = 'tag:Name'  
 $filter.Value = 'TeamCity'  
 $volumeId = (Get-EC2Volume -Filter $filter).VolumeId
 Add-EC2Volume $volumeId $instanceId xvdf
+
+#pull latest configuration
+cd d:\config\projects
+git pull
+
 # start TeamCity
 start-service TeamCity
-
-# set instance name
-New-EC2Tag -ResourceId $instanceId -Tag (new-object Amazon.EC2.Model.Tag).WithKey('Name').WithValue("ci.$($config.DomainName)")
-.\Register-CNAME.ps1 $config "ci.$($config.DomainName)" $cname
 
 # search computer by name
 Invoke-WmiMethod -path Win32_NetworkAdapterConfiguration -Name SetDNSSuffixSearchOrder -ArgumentList @($config.DomainName)
 
 if($config.fitnesse -ne $null) {
-  # set instance name
-  New-EC2Tag -ResourceId $instanceId -Tag (new-object Amazon.EC2.Model.Tag).WithKey('Name').WithValue("fitnesse.$($config.DomainName)")
-  .\Register-CNAME.ps1 $config "fitnesse.$($config.DomainName)" $cname
   cd c:\
   # clone PowerSlim
   git clone https://github.com/konstantinvlasenko/PowerSlim.git
