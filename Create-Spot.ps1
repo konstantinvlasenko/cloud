da27509d7ec5cb139683049e9d2f4876834b552b
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2015-02-10 14:07:08 -0500

    fix

--- Create-Spot.ps1
+++ Create-Spot.ps1
@@ -18,7 +18,7 @@ $role = Get-IAMInstanceProfileForRole $env:IAMRole
 
 if($env:SubnetId)
 {
-  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_SecurityGroup $env:SecurityGroup -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64 -LaunchSpecification_SubnetId $env:SubnetId
+  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_AllSecurityGroup $env:SecurityGroup -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64 -LaunchSpecification_SubnetId $env:SubnetId
 }
 else
 {
