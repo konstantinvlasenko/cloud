4109f319357ba4b092d6b1f989278c49dc3bb189
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2015-04-28 22:57:14 -0400

    KeyPait

--- Create-Spot.ps1
+++ Create-Spot.ps1
@@ -10,6 +10,7 @@ Param(
 Set-DefaultAWSRegion $env:AWSRegion
 
 if($env:EC2UserData) { $userdata64 = [System.Convert]::ToBase64String( [System.Text.Encoding]::UTF8.GetBytes( $env:EC2UserData ) ) }
+if($env:EC2KeyName -eq $null) { $env:EC2KeyName = (Get-EC2KeyPair)[0].KeyName }
 
 $role = Get-IAMInstanceProfileForRole $env:IAMRole
 
@@ -17,11 +18,11 @@ if($env:SubnetId)
 {
   $sg = new-object Amazon.EC2.Model.GroupIdentifier
   $sg.GroupId = $env:SecurityGroup
-  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_AllSecurityGroup $sg -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64 -LaunchSpecification_SubnetId $env:SubnetId
+  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_AllSecurityGroup $sg -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64 -LaunchSpecification_SubnetId $env:SubnetId -LaunchSpecification_KeyName $env:EC2KeyName
 }
 else
 {
-  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_SecurityGroup $env:SecurityGroup -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64
+  $spot =  Request-EC2SpotInstance -SpotPrice $env:SpotPrice -LaunchSpecification_InstanceType $type -LaunchSpecification_ImageId $ami -LaunchSpecification_SecurityGroup $env:SecurityGroup -IamInstanceProfile_Arn $role.Arn -LaunchSpecification_UserData $userdata64 -LaunchSpecification_KeyName $env:EC2KeyName
 }
 
 $spot.SpotInstanceRequestId
