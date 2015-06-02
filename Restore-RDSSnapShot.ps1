dd7d127324d253d4f131974b6e8608ed2146184e
Konstantin Vlasenko <konstantin.vlasenko@gmail.com>
2015-06-02 11:55:30 -0400

    restore RDS from snapshot

---
+++ Restore-RDSSnapShot.ps1
@@ -0,0 +1,23 @@
+Param(
+  [parameter(Mandatory=$true)]
+  $Identifier,
+  [parameter(Mandatory=$true)]
+  $snapshot,
+  [parameter(Mandatory=$true)]
+  [string]
+  $subnetGroupName,
+  [parameter(Mandatory=$true)]
+  [string]
+  $type
+)
+
+Set-DefaultAWSRegion $env:AWSRegion
+
+Restore-RDSDBInstanceFromDBSnapshot -DBInstanceIdentifier $Identifier -DBSnapshotIdentifier $snapshot -DBInstanceClass $type -DBSubnetGroupName $subnetGroupName
+"wait for instances running..." | Out-Default 
+do{
+	Sleep 30
+}while(Get-RDSDBInstance -DBInstanceIdentifier $Identifier).DBInstanceStatus -ne 'available' )
+
+
+ 
