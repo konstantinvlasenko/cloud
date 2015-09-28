3ebcf01152b43a1419d01ea64f0dc08910743c95
Henry Han <henry.han@software.dell.com>
2015-09-27 21:58:20 -0400

    verify spot id

--- Get-SpotInstance.ps1
+++ Get-SpotInstance.ps1
@@ -1,5 +1,12 @@
 Param($spotId)
 
 Set-DefaultAWSRegion $env:AWSRegion
-$spot = Get-EC2SpotInstanceRequest $spotId
+if($spotId)
+{
+  $spot = Get-EC2SpotInstanceRequest -SpotInstanceRequestId $spotId
+}
+else
+{
+   Write-Error "The spot id should not be null or empty"
+}
 (Get-EC2Instance $spot.InstanceId).RunningInstance
--- Set-SpotInstanceTags.ps1
+++ Set-SpotInstanceTags.ps1
@@ -2,7 +2,14 @@ Param($spotId)
 
 Begin {
   Set-DefaultAWSRegion $env:AWSRegion
+  if($spotId)
+  {
   $spot = Get-EC2SpotInstanceRequest -SpotInstanceRequestId $spotId
+  }
+  else
+  {
+   Write-Error "The spot id should not be null or empty"
+  }
 }
 
 Process {
